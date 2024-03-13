df0b996870c01e454099efce99dd14ba53c2548b
lucas <lucasvad123@gmail.com>
2024-03-13 17:08:27 -0500

    good work

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -64,7 +64,7 @@ export default function Home() {
     gsap.set(siesText.current, {xPercent: xPercent})
 
     requestAnimationFrame(animate);
-    xPercent += 0.1 * direction;
+    xPercent += 0.019 * direction;
   }
 
   return (
@@ -78,42 +78,24 @@ export default function Home() {
           /> 
       </div>
         
-      
-      <div className={styles.sliderContainer}>
-        <div ref={sliderz} className={styles.slider}>
-          <p ref={firstText}>LUCAS VADLAMUDI -</p>
-          <p ref={secondText}>LUCAS VADLAMUDI -</p> 
-        </div>
-        <div ref={sliderz} className={styles.slider}>
-          <p ref={thirdText}>LUCAS VADLAMUDI -</p>
-          <p ref={fourthText}>LUCAS VADLAMUDI -</p>
-        </div>
-        <div ref={sliderz} className={styles.slider}>
-          <p ref={fifthText}>LUCAS VADLAMUDI -</p>
-          <p ref={sixthText}>LUCAS VADLAMUDI -</p>
-        </div>
+      <div>
+        <div className={styles.sliderContainer}>
+          
+          <div ref={sliderz} className={styles.slider}>
+            <p ref={fifthText}>ENGINEER DESIGNER PROGRAMMER ‎ </p>
+            <p ref={sixthText}>ENGINEER DESIGNER PROGRAMMER ‎ </p>
+          </div>
 
-        <div ref={sliderz} className={styles.stroke}>
-          <p ref={unoText}>LUCAS VADLAMUDI -</p>
-          <p ref={dosText}>LUCAS VADLAMUDI -</p>
-        </div>
-        <div ref={sliderz} className={styles.stroke}>
-          <p ref={tresText}>LUCAS VADLAMUDI -</p>
-          <p ref={quatroText}>LUCAS VADLAMUDI -</p>
-        </div>
-        <div ref={sliderz} className={styles.stroke}>
-          <p ref={cincoText}>LUCAS VADLAMUDI -</p>
-          <p ref={siesText}>LUCAS VADLAMUDI -</p>
+          
+          
         </div>
       </div>
-
 
       <div data-scroll data-scroll-speed={0.1} className={styles.description}>
         <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z" fill="white"/>
         </svg>
-        <p>beebo</p>
-        <p>Dasdasdasd</p>
+        
       </div>
     </motion.main>
   )
